5dce12fade00ec21591bbfd5ec019b79ce8e353d
Mu-LINE <48792291+Mu-LINE@users.noreply.github.com>
2019-05-08 16:44:41 +0900

    Create Calculator.java
    授業ノート3

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+class Calculator {
+  public static void main(String[] args){
+    System.out.ptintln(6 + 6);
+    System.out.ptintln(6 - 6);
+    System.out.ptintln(6 * 6);
+    System.out.ptintln(6 / 6);
+    System.out.ptintln(6 % 6);
+  }
+}
