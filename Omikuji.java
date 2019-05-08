8b8cb0a7a8cb2a5ec6d3f5c8015bb9beebbc0fef
Mu-LINE <48792291+Mu-LINE@users.noreply.github.com>
2019-05-08 16:58:09 +0900

    Create Omikuji.java

---
+++ Omikuji.java
@@ -0,0 +1,22 @@
+import java.util.Random
+
+class Omikuji {
+   public static void main(String[] args){
+   Random rnd = new Random();
+      swicth(i){
+      case 1:
+        String unse = "大吉";
+        break;
+      case 2:
+      case 3:
+      case 4:
+        String unse = "小吉";
+        break;
+      case 5:
+        String unse = "大凶";
+        break;
+      defalt:
+        String unse = "吉";
+      }
+   }
+}
